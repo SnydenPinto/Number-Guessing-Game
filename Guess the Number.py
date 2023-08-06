2aa1f20ce39f434660973b0c4dae2a1862f93197
SNYDEN PINTO <2229231@chester.ac.uk>
2023-08-06 14:34:03 +0100

    added hint function, tells the users if the random number is even or odd

--- Guess the Number.py
+++ Guess the Number.py
@@ -1,9 +1,24 @@
 import random
-random_number = random.randrange(0,9)
+
+random_number = random.randrange(1, 9)
 
 attempts = 0
+numberOfHints = 3
 
 guess = int(input("Guess the number :"))
+
+
+def hints():
+    global numberOfHints
+    if numberOfHints > 0:
+        if random_number % 2 == 0:
+            print("The secret number is even")
+        else:
+            print("The secret number is odd")
+        numberOfHints -= 1
+    else:
+        print("You have used all your hints.")
+
 
 while guess != random_number:
     if guess < random_number:
@@ -16,6 +31,16 @@ while guess != random_number:
         attempts += 1
     else:
         break
-print("You gassed the number")
 
-print("it took you "+str(attempts)+" attempts to guess the correct number")
+    while attempts % 2 == 0:
+        user_hints =input(("Would you like to use a hint? yes or no"))
+        if user_hints == "yes":
+            hints()
+        else:
+            break
+        print("you got " + str(numberOfHints) + " hints left")
+
+
+
+print("You guessed the number")
+print("It took you " + str(attempts) + " attempts to guess the correct number")
