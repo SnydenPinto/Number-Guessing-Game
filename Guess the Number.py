f74cbeeb1990d92d632d540fc6dff36e2da600f7
SNYDEN PINTO <2229231@chester.ac.uk>
2023-08-06 14:01:29 +0100

    First commit

---
+++ Guess the Number.py
@@ -0,0 +1,20 @@
+import random
+random_number = random.randrange(0,9)
+
+attempts = 0
+
+guess = int(input("Guess the number :"))
+
+while guess != random_number:
+    if guess < random_number:
+        print("Too low")
+        guess = int(input("Guess the number :"))
+        attempts += 1
+    elif guess > random_number:
+        print("Too High")
+        guess = int(input("Guess the number :"))
+        attempts += 1
+    else:
+        break
+print("You gassed the number")
+print("it took you "+str(attempts)+" attempts to guess the correct number")
